079c3c109b577fa5e65602ff55d930c582e88347
Daniel Lopes <ddanielssoares@gmail.com>
2022-04-18 21:40:19 -0300

    pass by reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <gl/glut.h>
 #include "Emulator.h"
-#include "Gpu.h"
 #include "configs.h"
 #include "cpu.h"
 
@@ -9,11 +8,10 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	Cpu *cpu = new Cpu();
-	Gpu *graphics = new Gpu(640, 320, &title, cpu, keysMap);
 
 	//Emulator *emulator = new Emulator(640, 320, argv[1]);
 	char game[] = "./games/invaders.c8";
-	Emulator *emulator = new Emulator(640, 320, game, cpu, keysMap);
+	Emulator *emulator = new Emulator(640, 320, game, &title, cpu, &keysMap);
 
     return 0;
 }
