6779f1c72598cbc018cd5b62de2524cf0fe507fb
Daniel Lopes <ddanielssoares@gmail.com>
2022-05-19 19:10:51 -0300

    Created audio module

--- src/audio.cpp
+++ src/audio.cpp
@@ -1,2 +1,22 @@
 #include "audio.h"
 
+void Audio::initTimer()
+{
+    timerRunning = true;
+    // BEEP
+    timerInit = std::chrono::steady_clock::now();
+}
+
+void Audio::resetTimer()
+{
+    timerInit = std::chrono::steady_clock::now();
+}
+void Audio::stopTimer()
+{
+    timerRunning = false;
+}
+
+bool Audio::isTimerRunning()
+{
+    return timerRunning;
+}
--- src/audio.h
+++ src/audio.h
@@ -1,8 +1,22 @@
 #pragma once
-class audio
+#include <chrono>
+
+
+class Audio
 {
 private:
+	bool timerRunning;
+	int duration;
+	std::chrono::steady_clock::time_point timeInitCycle, timerInit;
 
 public:
+	Audio();
+	~Audio();
+
+	void initTimer();
+	void resetTimer();
+	void stopTimer();
+	bool isTimerRunning();
+	int getTimerRemain();
 };
 
