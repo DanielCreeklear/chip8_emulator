b95e1c0c832d49ba8ff51e053cbe25903a3b5726
Daniel Lopes <ddanielssoares@gmail.com>
2022-05-26 21:49:11 -0300

    Beeper implemented in audio

--- src/audio.cpp
+++ src/audio.cpp
@@ -1,22 +1,71 @@
 #include "audio.h"
 
+Audio::Audio()
+{
+    timerRunning = false;
+    duration = 1000;
+    Hz = 440;
+    std::string wavBeep = "bleep.wav";
+    beeper = new Beeper(wavBeep, 10);
+}
+
+Audio::~Audio()
+{
+
+}
+
+void Audio::play()
+{
+    if (isTimerRunning())
+    {
+        if (isTimerFinished())
+        {
+            stopTimer();
+            stopBeep();
+        }
+    }
+    else
+    {
+        initTimer();
+        beep();
+    }
+}
+
+void Audio::beep()
+{
+    beeper->play();
+}
+
+void Audio::stopBeep()
+{
+    beeper->stop();
+}
+
 void Audio::initTimer()
 {
     timerRunning = true;
-    // BEEP
     timerInit = std::chrono::steady_clock::now();
 }
 
-void Audio::resetTimer()
-{
-    timerInit = std::chrono::steady_clock::now();
-}
 void Audio::stopTimer()
 {
-    timerRunning = false;
+    if (isTimerRunning()) timerRunning = false;
 }
 
 bool Audio::isTimerRunning()
 {
     return timerRunning;
+}
+
+bool Audio::isTimerFinished()
+{
+    int diff = duration_cast<std::chrono::milliseconds> (std::chrono::steady_clock::now() - timerInit).count();
+    if (diff > 16.6)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
--- src/audio.h
+++ src/audio.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <chrono>
+#include <string>
+#include <iostream>
+#include "Beeper.h"
 
 
 class Audio
@@ -7,16 +10,21 @@ class Audio
 private:
 	bool timerRunning;
 	int duration;
+	double Hz;
 	std::chrono::steady_clock::time_point timeInitCycle, timerInit;
+	Beeper* beeper;
 
 public:
 	Audio();
 	~Audio();
 
+	void beep();
+	void stopBeep();
+
+	void play();
 	void initTimer();
-	void resetTimer();
 	void stopTimer();
 	bool isTimerRunning();
-	int getTimerRemain();
+	bool isTimerFinished();
 };
 
